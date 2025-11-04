3b831a970a5bd59eb0cc84e947183d4a9a7aadfa
Outcomer <773021792e@gmail.com>
2025-11-04 20:58:21 +0100

    Fixes

---
+++ js/material-imports.js
@@ -0,0 +1,18 @@
+/**
+ * Material Web Components imports
+ * Only imports the components used in the application for optimal performance
+ */
+
+// Buttons
+import '@material/web/button/filled-button.js';
+import '@material/web/iconbutton/icon-button.js';
+
+// Form controls
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/slider/slider.js';
+import '@material/web/textfield/outlined-text-field.js';
+import '@material/web/select/outlined-select.js';
+import '@material/web/select/select-option.js';
+
+// Icons
+import '@material/web/icon/icon.js';
